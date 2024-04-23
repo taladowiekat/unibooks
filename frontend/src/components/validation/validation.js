c1699ddc71500f173f291146ea558a0167298165
Momin Amarna <s12217072@stu.najah.edu>
2024-04-23 16:01:47 +0300

    i replace "Yup" to "yup" because "Yup" made run time error

--- frontend/src/components/validation/validation.js
+++ frontend/src/components/validation/validation.js
@@ -1,32 +1,32 @@
 // validation.js
 
-import * as Yup from 'yup';
+import * as yup from 'yup';
 
-export const createPostValidationSchema = Yup.object({
-  bookName: Yup.string().required('Book name is required'),
-  listingType: Yup.string().required('Listing type is required'),
-  images: Yup.array()
+export const createPostValidationSchema = yup.object({
+  bookName: yup.string().required('Book name is required'),
+  listingType: yup.string().required('Listing type is required'),
+  images: yup.array()
     .min(1, 'Please upload at least one image')
     .max(4, 'You can upload up to 4 images')
 });
 
-export const signInValidationSchema = Yup.object().shape({
-  emailOrUniversityId: Yup.string()
+export const signInValidationSchema = yup.object().shape({
+  emailOrUniversityId: yup.string()
     .matches(/^s\d{8}@stu\.najah\.edu$|^\d{8}$/, 'Invalid email or university ID format')
     .required('Email or university ID is required'),
-  password: Yup.string()
+  password: yup.string()
     .required('Password is required')
     .min(6, 'Password must be at least 6 characters long')
     .max(30, 'Password must be at most 30 characters long')    
 });
 
-export const signUpValidationSchema = Yup.object().shape({
-  universityId: Yup.string().matches(/^\d{8}$/, 'Invalid university ID format').required('University ID is required'),
-  email: Yup.string().matches(/^s\d{8}@stu\.najah\.edu$/, 'Invalid Email format').required('Email is required'),
-  password: Yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long').max(30, 'Password must be at most 30 characters long'),
-  confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match').required('Please confirm your password'),
+export const signUpValidationSchema = yup.object().shape({
+  universityId: yup.string().matches(/^\d{8}$/, 'Invalid university ID format').required('University ID is required'),
+  email: yup.string().matches(/^s\d{8}@stu\.najah\.edu$/, 'Invalid Email format').required('Email is required'),
+  password: yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long').max(30, 'Password must be at most 30 characters long'),
+  confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Please confirm your password'),
 });
 
-export const resetPasswordValidationSchema = Yup.object().shape({
-  email: Yup.string().matches(/^s\d{8}@stu\.najah\.edu$/, 'Invalid Email format').required('Email is required'),
+export const resetPasswordValidationSchema = yup.object().shape({
+  email: yup.string().matches(/^s\d{8}@stu\.najah\.edu$/, 'Invalid Email format').required('Email is required'),
 });
