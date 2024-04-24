d0873e9ff15d08f8b0fc6b7a047ab888c3176879
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2024-04-24 20:20:58 +0300

    applied formik to profile page

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -12,6 +12,8 @@ import {
 import EditIcon from '@mui/icons-material/Edit';
 import userImage from './profilepic.png';
 import RecoveryPopup from './RecoveryPopup.js';
+import { Field, Form, Formik } from 'formik';
+import { signUpValidationSchema } from '../../components/validation/validation.js';
 
 
 const ProfileForm = () => {
@@ -19,95 +21,138 @@ const ProfileForm = () => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const initialValues = {
+        // connect with database to get student's info
+        name: "",
+        college: "",
+        email: "",
+        password: "",
+    };
+
+    const handleSubmit = () => {
+        // if password matches with user, change info in database
+    };
+
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Paper sx={{ position: 'relative', width: 'fit-content', p: 2, mb: 4 }}>
-                    <Avatar src={userImage} sx={{ width: 120, height: 120 }} />
-                    <IconButton sx={{ position: 'absolute', top: 0, right: 0 }} size="small">
-                        <EditIcon />
-                    </IconButton>
-                </Paper>
+        <Formik
+            initialValues={initialValues}
+            onSubmit={handleSubmit}
+            validationSchema={signUpValidationSchema}
+        >
+            {({ errors, touched, isSubmitting }) => (
 
-                <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
-                    General
-                </Typography>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="name"
-                    label="Name"
-                    name="name"
-                    autoComplete="name"
-                    autoFocus
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="college"
-                    label="College Name"
-                    name="college"
-                    autoComplete="college"
-                />
+                <Form>
 
-                <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', fontSize: '1.5rem', marginTop: '1rem' }}>
-                    Security
-                </Typography>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="email"
-                    label="Email"
-                    type="email"
-                    id="email"
-                    autoComplete="email"
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
+                    <Container maxWidth="sm">
+                        <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <Paper sx={{ position: 'relative', width: 'fit-content', p: 2, mb: 4 }}>
+                                <Avatar src={userImage} sx={{ width: 120, height: 120 }} />
+                                <IconButton sx={{ position: 'absolute', top: 0, right: 0 }} size="small">
+                                    <EditIcon />
+                                </IconButton>
+                            </Paper>
 
-                <Button
-                    variant="outlined"
-                    color="secondary"
-                    sx={{ mt: 2 }}
-                    onClick={handleOpen}
-                >
-                    Change Password
-                </Button>
+                            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', fontSize: '1.5rem' }}>
+                                General
+                            </Typography>
+                            <Field
+                                as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                                name="name"
+                                autoComplete="name"
+                                autoFocus
+                            />
+                            <Field
+                                as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="college"
+                                label="College Name"
+                                name="college"
+                                autoComplete="college"
+                            />
 
-                <RecoveryPopup open={open} handleClose={handleClose} />
+                            <Typography variant="h6" gutterBottom sx={{ fontWeight: 'bold', fontSize: '1.5rem', marginTop: '1rem' }}>
+                                Security
+                            </Typography>
+                            <Field
+                                as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="email"
+                                label="Email"
+                                type="email"
+                                id="email"
+                                autoComplete="email"
 
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        sx={{ mr: 1 }}
-                    >
-                        Save
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        color="secondary"
-                        sx={{ ml: 1 }}
-                    >
-                        Cancel
-                    </Button>
-                </Box>
-            </Box>
+                                error={
+                                    touched.email && Boolean(errors.email)
+                                }
+                                helperText={
+                                    touched.email ? errors.email : ""
+                                }
+                            />
+                            <Field
+                                as={TextField}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+
+                                error={
+                                    touched.password && Boolean(errors.password)
+                                }
+                                helperText={
+                                    touched.password ? errors.password : ""
+                                }
+                            />
+
+                            <Button
+                                variant="outlined"
+                                color="secondary"
+                                sx={{ mt: 2 }}
+                                onClick={handleOpen}
+                            >
+                                Change Password
+                            </Button>
+
+                            <RecoveryPopup open={open} handleClose={handleClose} />
+
+                            <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="primary"
+                                    sx={{ mr: 1 }}
+                                >
+                                    Save
+                                </Button>
+                                <Button
+                                    variant="outlined"
+                                    color="secondary"
+                                    sx={{ ml: 1 }}
+                                >
+                                    Cancel
+                                </Button>
+                            </Box>
+                        </Box>
 
 
-        </Container>
+                    </Container>
+
+                </Form>
+            )}
+        </Formik>
     );
 };
 
