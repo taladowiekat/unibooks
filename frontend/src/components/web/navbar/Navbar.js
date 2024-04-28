4ef7ecbdc7c80c7293b973807b273d7db27c8b09
Momin Amarna <s12217072@stu.najah.edu>
2024-04-28 04:49:26 +0300

    Edit navbar and footer design

--- frontend/src/components/web/navbar/Navbar.js
+++ frontend/src/components/web/navbar/Navbar.js
@@ -1,81 +1,111 @@
-import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, Menu, MenuItem, Box, useTheme, useMediaQuery } from '@mui/material';
+import * as React from 'react';
+import { useState } from 'react';
+import {
+    Box, AppBar, Toolbar, Button,
+    Divider, Typography, MenuItem, Drawer,
+    Container,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import logo from '../../../assets/Small_Unibooks_Logo__Name.png'
-const Navbar = () => {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+import { Link } from 'react-router-dom';
 
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
+
+function Navbar() {
+    const [open, setOpen] = useState(false);
+    const toggleDrawer = (newOpen) => () => {
+        setOpen(newOpen);
     };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-    useMediaQuery(theme.breakpoints.up('md'), {
-        noSsr: true,
-        onChange: matches => {
-            if (!matches) {
-                setAnchorEl(null);
-            }
-        },
-    });
 
     return (
-        <AppBar component="nav" color="default" elevation={0}>
-            <Toolbar>
-                <Box display="flex" alignItems="center">
-                    <Typography variant="h6" color="inherit" noWrap sx={{ margin: '0 16px' }}>
-                    <img src={logo} alt="UNIBOOKS LOGO" style={{ maxHeight: '50px' }} />
-                    </Typography>
-                </Box>
-                <Box flexGrow={1} display="flex" justifyContent="center">
-                    {!isMobile && (
-                        <>
-                            <Button color="inherit">Home</Button>
-                            <Button color="inherit">Contact Us</Button>
-                            <Button color="inherit">All Posts</Button>
-                        </>
-                    )}
-                </Box>
-
-                {!isMobile && (
-                    <Box>
-                        <Button variant="outlined" sx={{ margin: '0 8px' }}>Log-In</Button>
-                        <Button variant="contained" disableElevation>Sign-Up</Button>
-                    </Box>
-                )}
-                {isMobile && (
-                    <IconButton edge="end" color="inherit" onClick={handleMenu} aria-label="Menu">
-                        <MenuIcon />
-                    </IconButton>
-                )}
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }}
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose}>Home</MenuItem>
-                    <MenuItem onClick={handleClose}>Contact Us</MenuItem>
-                    <MenuItem onClick={handleClose}>All Posts</MenuItem>
-                    <MenuItem onClick={handleClose}>Log-In</MenuItem>
-                    <MenuItem onClick={handleClose}>Sign-Up</MenuItem>
-                </Menu>
-            </Toolbar>
-        </AppBar>
+        <>
+            <AppBar position='absolute' color='default' sx={{ bottom:'auto' }} >
+                <Container maxWidth={false}  >
+                    <Toolbar >
+                        <Box
+                            sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', }}>
+                            <img src='Small_Unibooks_Logo__Name.png' alt='UNIBOOKS LOGO'
+                                style={{ maxHeight: '45px' }} />
+                            <Box sx={{
+                                display: { xs: 'none', md: 'flex' },
+                                justifyContent: 'center', flexGrow: 1,
+                            }}>
+                                <MenuItem sx={{ py: '6px', px: '12px' }}
+                                    component={Link} to='/' >
+                                    <Typography variant='body' color='text.primary'>
+                                        Home
+                                    </Typography>
+                                </MenuItem>
+                                <MenuItem sx={{ py: '6px', px: '12px' }}
+                                    component={Link} to='allPosts'>
+                                    <Typography variant='body' color='text.primary'>
+                                        All Posts
+                                    </Typography>
+                                </MenuItem>
+                                <MenuItem sx={{ py: '6px', px: '12px' }} component={Link}
+                                    to='contactUs'>
+                                    <Typography variant='body' color='text.primary'>
+                                        Contact Us
+                                    </Typography>
+                                </MenuItem>
+                            </Box>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: { xs: 'none', md: 'flex' },
+                                gap: 0.5, alignItems: 'center'
+                            }}>
+                            <Button color='primary' variant='outlined'
+                                component={Link} to='login' >
+                                Sign in
+                            </Button>
+                            <Button color='primary' variant='outlined'
+                                component={Link} to='register' >
+                                Sign up
+                            </Button>
+                        </Box>
+                        <Box sx={{ display: { md: 'none' } }}>
+                            <Button variant='text' color='primary' aria-label='menu'
+                                onClick={toggleDrawer(true)} sx={{ minWidth: '30px', p: '4px' }} >
+                                <MenuIcon sx={{ color:'black' }} />
+                            </Button>
+                            <Drawer anchor='right' open={open} onClose={toggleDrawer(false)}>
+                                <Box sx={{
+                                    minWidth: '35vw ', p: 2,
+                                    backgroundColor: 'background.paper', flexGrow: 1,
+                                }} >
+                                    <Box sx={{
+                                        display: 'flex', flexDirection: 'column',
+                                        alignItems: 'end', flexGrow: 1,
+                                    }} >
+                                    </Box>
+                                    <MenuItem>
+                                        Home
+                                    </MenuItem>
+                                    <MenuItem component={Link} to='allPosts'>
+                                        All Posts
+                                    </MenuItem>
+                                    <MenuItem component={Link} to='contactUs'>
+                                        Contact us
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem>
+                                        <Button color='primary' variant='outlined'
+                                            component={Link} to='register'>
+                                            Sign up
+                                        </Button>
+                                    </MenuItem>
+                                    <MenuItem>
+                                        <Button color='primary' variant='outlined'
+                                            component={Link} to='login' >
+                                            Sign in
+                                        </Button>
+                                    </MenuItem>
+                                </Box>
+                            </Drawer>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
 };
 
