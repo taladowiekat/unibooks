21a0714c9db5ca1328ef7b67d9abfe6835035ca4
Dana-Morgan <dana.morgan196@gmail.com>
2024-05-01 12:42:05 +0300

    edit path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './app.css'
 import { RouterProvider } from 'react-router-dom';
 import { router } from './layouts/routes.js';
-import CreateListing from './components/web/createPost/CreatPost.js';
 
 function App() {
   return (
     <>
-<CreateListing/>    </>
+    <RouterProvider router={router} />
+    </>
   );
 }
 
