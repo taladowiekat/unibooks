a399201849ee344cd4f9fd0eb254bcb9801fce6e
Momin Amarna <s12217072@stu.najah.edu>
2024-05-01 14:53:41 +0300

    in foter replace container to appbar and edit pading in navbar : replace absolute position to fixed position

--- frontend/src/components/web/footer/Footer.js
+++ frontend/src/components/web/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, IconButton, Grid, AppBar, Container } from '@mui/material';
+import { Typography, IconButton, Grid, Container, AppBar } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TelegramIcon from '@mui/icons-material/Telegram';
@@ -7,33 +7,40 @@ import EmailIcon from '@mui/icons-material/Email';
 
 const Footer = () => {
     return (
-        <AppBar position='absolute' color = 'default' elevation='16' sx={{ top: 'auto' }}   >
-            <Container maxWidth={false} sx={{ p: 1 }}>
-                <Grid container alignItems='center' justifyContent='space-between'>
-                    <Grid item>
-                        <img src= 'Small_Unibooks_Logo__Name.png' alt='UNIBOOKS LOGO' style={{ maxHeight: '45px' }} />
-                        <Typography sx={{ ml: 6, color: 'GrayText' }}>
-                            SELL, EXCHANGE, DONATE
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <IconButton href='https://instagram.com'>
-                            <InstagramIcon fontSize='large' />
-                        </IconButton>
-                        <IconButton href='https://facebook.com'>
-                            <FacebookIcon fontSize='large' />
-                        </IconButton>
-                        <IconButton href='https://telegram.org'>
-                            <TelegramIcon fontSize='large' />
-                        </IconButton>
-                        <IconButton href='mailto:email@example.com'>
-                            <EmailIcon fontSize='large' />
-                        </IconButton>
-                    </Grid>
+        <AppBar position='fixed' maxWidth={false} color='default'
+            sx={{
+                width: '100%', 
+                top:'auto',
+                bottom: 0, 
+                boxShadow: '0px -5px 7px -5px rgba(0, 0, 0, 0.5)',
+                flexGrow: 1,
+                display: 'flex',
+            }}>
+            <Grid container alignItems='center' justifyContent='space-between'>
+                <Grid item >
+                    <img src='Small_Unibooks_Logo__Name.png' alt='UNIBOOKS LOGO' style={{ paddingLeft:'15px' , maxHeight: '45px' }} />
+                    <Typography sx={{ ml: 6, color: 'GrayText' }}>
+                        SELL, EXCHANGE, DONATE
+                    </Typography>
                 </Grid>
-            </Container>
+                <Grid item style={{paddingRight: '13px' }} >
+                    <IconButton href='https://instagram.com'>
+                        <InstagramIcon fontSize='large' />
+                    </IconButton>
+                    <IconButton href='https://facebook.com'>
+                        <FacebookIcon fontSize='large' />
+                    </IconButton>
+                    <IconButton href='https://telegram.org'>
+                        <TelegramIcon fontSize='large' />
+                    </IconButton>
+                    <IconButton href='mailto:email@example.com'>
+                        <EmailIcon fontSize='large' />
+                    </IconButton>
+                </Grid>
+            </Grid>
         </AppBar>
     );
 }
 
 export default Footer;
+
