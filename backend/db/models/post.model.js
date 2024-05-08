d471103f8063ab993793c137800983b3749531de
Momin Amarna <s12217072@stu.najah.edu>
2024-05-08 19:51:34 +0300

    post model almost complete

---
+++ backend/db/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema({
+    studentID: {
+        type: Schema.Types.ObjectId, ref: 'User',
+    },
+    bookImage: {
+        type: String,
+    },
+    bookName: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+    },
+    postType: {
+        type: String,
+        enum: ['Sell', 'Donate', 'Exchange']
+    },
+    exchangeBookName: {
+        type: String,
+        default: null
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive']
+    },
+
+}, { timestamps: true });
+
+
+const postModel = model('Post', postSchema);
+
+export default postModel;
