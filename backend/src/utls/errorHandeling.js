ba850e616a3867555c21611554b9c471f58cf782
Dana-Morgan <dana.morgan196@gmail.com>
2024-05-16 14:03:17 +0300

    Adding asyncHandler from Express and using it in my files

--- backend/src/utls/errorHandeling.js
+++
@@ -1,8 +0,0 @@
-export const asyncHandler = (fun)=>{
-return (req, res,next)=>{
-    fun(req, res,next).catch(err=>{
-        return res.json({message:"catch error",error : err.stack});
-    })
-    next()
-}
-}
