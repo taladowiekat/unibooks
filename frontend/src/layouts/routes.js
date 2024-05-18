ecd1d983961f3054551213ca6e6b5bafad892e67
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2024-05-18 13:36:44 +0300

    add routes for most pages (home TBA)

--- frontend/src/layouts/routes.js
+++ frontend/src/layouts/routes.js
@@ -4,29 +4,47 @@ import Layout from './Layout.js';
 import Login from '../pages/logIn/LogIn.js';
 import Register from '../pages/register/Register.js';
 import Posts from '../pages/allPosts/Posts.js';
+import ResetPassword from '../pages/resetPassword/ResetPass.js';
+import ProfileForm from '../pages/profile/Profile.js';
+import ContactUs from '../pages/contactUs/ContactUs.js';
 
 
 
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-        {
-            path: 'register',
-            element: <Register/>
-        },
-        {
-            path: 'login',
-            element: <Login/>
-        },
-        // {
-        //     path: 'Home',
-        //     element: <Posts/>
-        // },
-        {
-            path: 'allPosts',
-            element: <Posts/>
-        }]}
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: 'register',
+                element: <Register />
+            },
+            {
+                path: 'login',
+                element: <Login />
+            },
+            // {
+            //     path: 'Home',
+            //     element: <Posts/>
+            // },
+            {
+                path: 'allPosts',
+                element: <Posts />
+            },
+            {
+                path: 'resetPass',
+                element: <ResetPassword />
+            },
+            {
+                path: 'profile',
+                element: <ProfileForm />
+            },
+            {
+                path: 'contactUs',
+                element: <ContactUs />
+            }
+
+        ]
+    }
 ]);
