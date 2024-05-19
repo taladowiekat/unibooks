29b181406a4edaf5e526812f34e02c7e33e333bb
tala dowiekat <taladweikat2017@gmail.com>
2024-05-20 00:37:37 +0300

    handel some issues

--- backend/src/modules/app.router.js
+++ backend/src/modules/app.router.js
@@ -1,5 +1,6 @@
 import cors from "cors";
 import dbconnection from '../../db/connection.js'
+import authRouter from './auth/auth.router.js';
 import postRouter from './posts/posts.router.js';
 
 const initApp = (app, express) => {
@@ -16,8 +17,11 @@ const initApp = (app, express) => {
 
     //all routers
 
+    app.use('/auth' , authRouter)
 
     app.use('/post', postRouter)
+
+
     
     app.use("*", (req, res) => {
         return res.status(404).json({ message: "page not found" });
