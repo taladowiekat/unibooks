9e18cba40f94ae1bfe3a5b8b32138f5a4f2fd54a
aisha saleh <aisha.mohd.slaeh@gmail.com>
2024-05-24 20:34:35 +0300

    remove the Cards commponent menu that have delete/edit button

--- frontend/src/components/shared/Cards.js
+++ frontend/src/components/shared/Cards.js
@@ -1,58 +1,18 @@
-import React, { useState } from 'react';
+
 import { Card, CardContent, CardMedia, Avatar, Typography, Button, Box, IconButton } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import ShareIcon from '@mui/icons-material/Share';
-import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 function PostCard({ userAvatar, userName, bookName, bookType, image, typeoperation }) {
-    const [anchorEl, setAnchorEl] = useState(null);
+
     const {t}=useTranslation();
-    const open = Boolean(anchorEl);
-    const handleMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-      };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+   
  
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', p: '1rem', flexDirection: 'column' }}>
             <Card sx={{ maxWidth: 360, borderRadius: '16px', boxShadow: 3 }}>
-            <Button
-                    sx={{ marginLeft: "80%" }}
-                    id="basic-button"
-                    aria-haspopup="true"
-                    onClick={handleMenuOpen}
-                >
-                    {<MoreVertIcon />}
-                </Button>
-                <Menu
-    sx={{ marginLeft:"2%" }}
-    id="basic-menu"
-    anchorEl={anchorEl}
-    open={open}
-    onClose={handleClose}
->
-    <MenuItem onClick={handleClose} component={Link} to='EditPost'>
-        <Button sx={{ color: "black" }}>
-            {<EditIcon  sx={{ color: "black", marginRight:"22%"}} />}
-            {t("editButton")}
-        </Button>
-    </MenuItem>
-    <MenuItem onClick={handleClose}>
-        <Button sx={{ color: "black" }}>
-            {<DeleteIcon sx={{ color: "black", marginRight:"20%" }}/>}
-            {t("deleteButton")}
-        </Button>
-    </MenuItem>
-</Menu>
-                <CardMedia
+           
+        <CardMedia
                     component="img"
                     sx={{
                         height: 180,
