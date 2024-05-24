6db1c6182b68db8ed13dc63de7baee19980ab743
aisha saleh <aisha.mohd.slaeh@gmail.com>
2024-05-24 20:39:45 +0300

    (backend) - Implemented deletePost function to handle the deletion of posts - The function checks if the post exists and verifies user authorization - Sends response messages based on the outcome - Add route to handle delete requests for post deletion   - Route: DELETE /remove/:id   - Middleware: auth('user') for user authorization

--- backend/src/modules/posts/posts.controller.js
+++ backend/src/modules/posts/posts.controller.js
@@ -1,7 +1,6 @@
 import postModel from '../../../db/models/post.model.js';
 import cloudinary from '../../utils/cloudinary.js';
 import slugify from 'slugify';
-
 //create a new post
 
 export const create = async (req, res) => {
@@ -28,5 +27,27 @@ export const create = async (req, res) => {
         const populatedPost = await postModel.findById(newPost._id).populate('studentID', 'firstname lastname email');
 
         return res.status(201).json({ message: 'Post created successfully', post: populatedPost });
+
+};
+
+//delete post
+export const deletePost = async (req, res) => {
+    const { id: postID } = req.params;
+    try {
+        const post = await postModel.findById(postID);
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
+        const userId = req.user._id;
+        if (post.studentID.toString() !== userId.toString()) {
+            return res.status(401).json({ message: 'User not authorized to delete this post' });
+        }
+
+        await postModel.findByIdAndDelete(postID);
+        res.status(200).send({ message: `Post with Id:${postID} has been deleted` });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 
 };
--- backend/src/modules/posts/posts.router.js
+++ backend/src/modules/posts/posts.router.js
@@ -6,5 +6,7 @@ import asyncHandler from 'express-async-handler';
 const router = Router();
 
 router.post('/create', auth('user'), fileUpload(fileType.image).array('image', 4), asyncHandler(controller.create));
+router.delete('/remove/:id' ,auth('user'),asyncHandler(controller.deletePost));
+
 
 export default router;
