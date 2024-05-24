c4f25cfb93d8288b3da4775c9ae7c8043c834d0b
Dana-Morgan <dana.morgan196@gmail.com>
2024-05-24 16:21:50 +0300

    rename controller properly

--- backend/src/modules/auth/auth.router.js
+++ backend/src/modules/auth/auth.router.js
@@ -1,16 +1,16 @@
 import {Router} from 'express';
-import * as AuthConroller from './auth.controller.js';
+import * as AuthController from './auth.controller.js';
 import validation from './../../middleware/validation.middleware.js';
 import { signinschema, signupschema } from './auth.validation.js';
 import asyncHandler from 'express-async-handler';
 
 const authRouter = Router();
 
-authRouter.post('/signup',validation(signupschema) ,asyncHandler(AuthConroller.signup))
-authRouter.post('/signin',validation(signinschema) ,asyncHandler(AuthConroller.signin))
+authRouter.post('/signup',validation(signupschema) ,asyncHandler(AuthController.signup))
+authRouter.post('/signin',validation(signinschema) ,asyncHandler(AuthController.signin))
 
-authRouter.patch('/forgotPassword',asyncHandler(AuthConroller.forgotPassword));
-authRouter.patch('/resetPassword',asyncHandler(AuthConroller.resetPassword));
-authRouter.patch('/changePassword',asyncHandler(AuthConroller.changePassword));
+authRouter.patch('/forgotPassword',asyncHandler(AuthController.forgotPassword));
+authRouter.patch('/resetPassword',asyncHandler(AuthController.resetPassword));
+authRouter.patch('/changePassword',asyncHandler(AuthController.changePassword));
 
 export default authRouter ;
