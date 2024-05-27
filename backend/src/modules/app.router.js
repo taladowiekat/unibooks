92cdb66d07fb5e849b9464d244ea8c9129184301
tala dowiekat <taladweikat2017@gmail.com>
2024-05-27 22:23:16 +0300

    fix issues

--- backend/src/modules/app.router.js
+++ backend/src/modules/app.router.js
@@ -4,11 +4,7 @@ import authRouter from './auth/auth.router.js';
 import postRouter from './posts/posts.router.js';
 import profileRouter from './profile/profile.router.js';
 import userRouter from './user/user.router.js';
-<<<<<<< HEAD
 import userMessage from './message/messageRoutes.js'
-=======
-
->>>>>>> 6ac70ed091608d19efe9d4dbfd485e632dc9d185
 
 const initApp = (app, express) => {
 
@@ -32,11 +28,7 @@ const initApp = (app, express) => {
 
     app.use('/user', userRouter);
 
-<<<<<<< HEAD
     app.use('/message', userMessage);
-=======
-
->>>>>>> 6ac70ed091608d19efe9d4dbfd485e632dc9d185
 
     
     app.use("*", (req, res) => {
@@ -45,3 +37,5 @@ const initApp = (app, express) => {
 
 
 };
+
+export default initApp;
