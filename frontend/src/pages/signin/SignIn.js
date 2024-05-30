3df6520f4f684af89e34ec6407b78579261decc2
Dana-Morgan <dana.morgan196@gmail.com>
2024-05-30 18:58:10 +0300

    fixed navigate in signin to redirect users to home page, not allPosts

--- frontend/src/pages/signin/SignIn.js
+++ frontend/src/pages/signin/SignIn.js
@@ -41,7 +41,7 @@ const LogIn = () => {
       });
 
       setUser(userResponse.data.user);
-      navigate('/allPosts');
+      navigate('/');
     } catch (error) {
       if (error.response) {
         if (error.response.status === 404) {
