14ec2ee139ca22282bae7f4486332f42fce01098
Abdul-Kareem Zaid <s12113676@stu.najah.edu>
2024-05-31 00:56:31 +0300

    rerolled last change to protected router

--- frontend/src/components/protectedRouter/ProtectedRouter.js
+++ frontend/src/components/protectedRouter/ProtectedRouter.js
@@ -1,16 +1,9 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { jwtDecode } from 'jwt-decode';
 
-function isTokenValid() {
-    const decodedtoken = jwtDecode(localStorage.getItem("token"))
-    if (decodedtoken && Date.now() >= decodedtoken.exp * 1000)
-        return false;
-    return true;
-}
 
 function ProtectedRouter({ children }) {
-    if (localStorage.getItem("token") == null || isTokenValid) {
+    if (localStorage.getItem("token") == null) {
         return <Navigate to='/login' />
     }
     return (
